92c9fa7ae0125ccf1b6b254f27b923b035efb143
Sanela <halilkovicsaanela@gmail.com>
2026-05-24 18:33:48 +0200

    commit

--- app/src/main/java/com/example/wearagain/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/wearagain/ui/home/HomeActivity.java
@@ -16,6 +16,9 @@ import com.example.wearagain.ui.settings.SettingsActivity;
 import com.example.wearagain.viewmodel.ProizvodViewModel;
 import com.example.wearagain.ui.product.ProizvodDetaljiActivity;
 import com.example.wearagain.ui.product.AddProizvodActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -33,6 +36,7 @@ public class HomeActivity extends AppCompatActivity {
         postaviViewModel();
         postaviPretragu();
         postaviNavigaciju();
+        dodajTestneProizvode();
     }
 
     private void postaviRecyclerView() {
@@ -80,5 +84,48 @@ public class HomeActivity extends AppCompatActivity {
 
         vezanje.btnKorpa.setOnClickListener(v ->
                 startActivity(new Intent(HomeActivity.this, KorpaActivity.class)));
+    }
+    private void dodajTestneProizvode() {
+        FirebaseDatabase.getInstance("https://wearagain-4f746-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference("proizvodi")
+                .addListenerForSingleValueEvent(new com.google.firebase.database.ValueEventListener() {
+                    @Override
+                    public void onDataChange(com.google.firebase.database.DataSnapshot snapshot) {
+                        if (snapshot.getChildrenCount() < 3) {
+                            ubaciTestneProizvode();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(com.google.firebase.database.DatabaseError error) {}
+                });
+    }
+    private void ubaciTestneProizvode() {
+        DatabaseReference ref = FirebaseDatabase.getInstance("https://wearagain-4f746-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference("proizvodi");
+
+        String[][] proizvodi = {
+                {"Zara ljetna haljina", "Prekrasna ljetna haljina, nosena samo jednom.", "35.0", "S", "Haljine", "Sarajevo", "Bosna i Hercegovina", "https://i.pinimg.com/1200x/e1/1a/e3/e11ae35310e6a7e234ea2ffa62b81be0.jpg"},
+                {"H&M hlače", "Plave baggy fit hlače, odlično stanje.", "25.0", "M", "Pantalone", "Mostar", "Bosna i Hercegovina", "https://i.pinimg.com/736x/81/6b/4a/816b4a5db313482f5510b4b9a41252bd.jpg"},
+                {"Nike sportska jakna", "Lagana sportska jakna, idealna za proljeće.", "45.0", "L", "Jakne", "Banja Luka", "Bosna i Hercegovina", "https://i.pinimg.com/1200x/fe/31/50/fe3150a3b1dec2ea91bc7b82469bf66f.jpg"},
+                {"Mango bijela majica", "Klasična bijela majica, idealna za svaku priliku.", "15.0", "S", "Majice", "Tuzla", "Bosna i Hercegovina", "https://i.pinimg.com/1200x/75/4a/02/754a02c8e6f0813f60ce747d8784ff4b.jpg"},
+                {"Tommy Hilfiger haljina", "Elegantna haljina za posebne prilike.", "60.0", "XS", "Haljine", "Zenica", "Bosna i Hercegovina", "https://i.pinimg.com/736x/9a/92/74/9a9274c741b9023327918f6b7d5d9e52.jpg"}
+        };
+
+        for (String[] p : proizvodi) {
+            String id = ref.push().getKey();
+            java.util.Map<String, Object> proizvod = new java.util.HashMap<>();
+            proizvod.put("id", id);
+            proizvod.put("naziv", p[0]);
+            proizvod.put("opis", p[1]);
+            proizvod.put("cijena", Double.parseDouble(p[2]));
+            proizvod.put("velicina", p[3]);
+            proizvod.put("kategorija", p[4]);
+            proizvod.put("grad", p[5]);
+            proizvod.put("drzava", p[6]);
+            proizvod.put("slikaUrl", p[7]);
+            proizvod.put("korisnikId", "testni_korisnik");
+            ref.child(id).setValue(proizvod);
+        }
     }
 }
